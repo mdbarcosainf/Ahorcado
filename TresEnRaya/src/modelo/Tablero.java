1fba4269098942b29f59c9a069f4d615a14bb56a
mdbarcosainf <76221440+mdbarcosainf@users.noreply.github.com>
2024-06-25 10:50:22 +0200

    Add files via upload

---
+++ TresEnRaya/src/modelo/Tablero.java
@@ -0,0 +1,15 @@
+package modelo;
+
+/**
+ *
+ * @author soledadbarco
+ */
+
+public interface Tablero {
+    void ver();
+    boolean poner(Posicion pos, char pieza);
+    void limpiar();
+    boolean quitar(Posicion pos, char pieza);
+    boolean tresEnRaya();
+    boolean lleno();
+}
