5454f830363ff80b1e697a5df1fc8dad884dd822
mdbarcosainf <76221440+mdbarcosainf@users.noreply.github.com>
2024-06-25 08:22:48 +0200

    Update Tablero.java

--- TresEnrayas/src/modelo/Tablero.java
+++ TresEnrayas/src/modelo/Tablero.java
@@ -6,10 +6,10 @@ package modelo;
  */
 
 public interface Tablero {
-    void ver();
+   /* void ver();
     boolean poner(Posicion pos, char marca);
     void limpiar();
     boolean quitar(Posicion pos, char marca);
     boolean tresEnRaya();
-    //boolean lleno();
+    boolean lleno();*/
 }
