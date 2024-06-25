787e476e6c7ea9efa06b5b2ec03649a0d1cd58e5
mdbarcosainf <76221440+mdbarcosainf@users.noreply.github.com>
2024-06-25 08:02:22 +0200

    Update ControladorJuego.java

--- TresEnrayas/src/controlador/ControladorJuego.java
+++ TresEnrayas/src/controlador/ControladorJuego.java
@@ -1,10 +1,21 @@
 package controlador;
-
+import vista.VistaJuego;
 /**
  *
  * @author soledadbarco
  */
 
 public class ControladorJuego {
+    private TableroJuego tablero;
+    private VistaJuego vista;
+    private Jugador[] jugadores;
+    private int turnoActual;
+
+    public ControladorJuego(TableroJuego tablero, VistaJuego vista, Jugador[] jugadores) {
+        this.tablero = tablero;
+        this.vista = vista;
+        this.jugadores = jugadores;
+        this.turnoActual = 0;
+    }
     
 }
